4d1c63dddd6fb0664ca456240162eb5f44a3881a
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-02-24 21:03:59 -0500

    create a logo

--- social-petgram/src/App.jsx
+++ social-petgram/src/App.jsx
@@ -1,11 +1,13 @@
 import { ListOfCategories } from "./Components/ListOfCategories"
 import { GlobalStyle } from "./GlobalStyles"
 import { ListOfPhotoCards } from "./Components/ListOfPhotoCards"
+import { Logo } from "./Components/Logo/Index"
 function App() {
 
   return (
     <div className="App">
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCards />
     </div>
